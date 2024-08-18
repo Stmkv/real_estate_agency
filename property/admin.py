93629614ae876de934f32aa98304f9b1980455a4
Stmkv <stepkamakarov1999@mail.ru>
2024-08-18 16:40:00 +0300

    add db model for owners

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -14,11 +14,17 @@ class FlatAdmin(admin.ModelAdmin):
     ]
     list_editable = ["new_building",]
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ('who_liked',)
 
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
 
 
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+
+
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
