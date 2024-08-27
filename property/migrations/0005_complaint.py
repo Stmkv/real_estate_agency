d9267272ba3ba03cb9e04bdfd84ecf577f333f38
Stmkv <stepkamakarov1999@mail.ru>
2024-08-27 19:03:19 +0300

    Убрал лишнюю миграцию

--- property/migrations/0005_complaint.py
+++ property/migrations/0005_complaint.py
@@ -6,20 +6,42 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('property', '0004_auto_20240818_1627'),
+        ("property", "0003_auto_20240818_1624"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Complaint',
+            name="Complaint",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('text', models.TextField(blank=True, null=True, verbose_name='Текст жалобы')),
-                ('flat', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='property.Flat', verbose_name='Квартира, на которую пожаловались')),
-                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='Кто жаловался')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("text", models.TextField(blank=True, null=True, verbose_name="Текст жалобы")),
+                (
+                    "flat",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="property.Flat",
+                        verbose_name="Квартира, на которую пожаловались",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="Кто жаловался",
+                    ),
+                ),
             ],
         ),
     ]
